87814f99f40c48440c9d51cfe6e2fc72dfb2aabf
Paul Harris <paul@trickbooter.com>
2024-05-15 21:21:57 +1000

    Make changes towards SniS hosted app

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@
 
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
 
 # Write the app title & header text
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
@@ -13,8 +13,8 @@ st.write("Choose the fruits you want in your custom Smoothie!\n")
 name_on_order = st.text_input('Name for order?')
 
 # Read fruit options from Snowflake
-from snowflake.snowpark.functions import col
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 f_df = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 
 # Offer the user a multiselect box for their fruit choices
